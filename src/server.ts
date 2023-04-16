245fe12b4a5a4cd76194cda4e6616dec5ac8bfad
oscarfgmaia <oscarfgmaia@gmail.com>
2023-04-16 00:02:55 -0300

    style: prerttier

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import registerValidate from "./middleware/registerValidate.js";
-
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import registerValidate from './middlewares/registerValidate.js';
+import userRouter from './routes/userRoutes.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.get("/health", (req, res) => res.send("oscar aqui OK HEALTH"));
-app.post("/sign-up", registerValidate, (req, res) => res.sendStatus(200));
-
+app.get('/health', (req, res) => res.send('oscar aqui OK HEALTH'));
+app.use(userRouter);
 const PORT = 5000;
 
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
